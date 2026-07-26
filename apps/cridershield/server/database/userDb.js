9f50d2d7144acc1e758a4b619d23558683c0b896
Jessie Crider (CriderGPT) <jessiecrider3@gmail.com>
2026-07-26 19:59:07 -0400

    Import CriderShield: server/database/userDb.js

---
+++ apps/cridershield/server/database/userDb.js
@@ -0,0 +1,11 @@
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
+const fs = require('fs');
+
+const dataDir = path.join(__dirname, '../../data');
+if (!fs.existsSync(dataDir)) { fs.mkdirSync(dataDir, { recursive: true }); }
+const db = new sqlite3.Database(path.join(dataDir, 'users.db'));
+db.serialize(() => {
+  db.run(`CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT UNIQUE, password TEXT)`);
+});
+module.exports = db;
