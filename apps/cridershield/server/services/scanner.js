6a47ef1dfef85d84d51d256dbc52826a0aadd9f9
Jessie Crider (CriderGPT) <jessiecrider3@gmail.com>
2026-07-26 20:35:21 -0400

    Integrate and harden CriderShield dashboard

--- apps/cridershield/server/services/scanner.js
+++ apps/cridershield/server/services/scanner.js
@@ -1,17 +1,17 @@
-const { exec } = require('child_process');
+const { execFile } = require('child_process');
 const deviceDb = require('../database/deviceDb');
 
 const scan = () => {
-  exec('arp -a', (err, stdout) => {
+  execFile('/usr/sbin/ip', ['neigh', 'show'], (err, stdout) => {
     if (err) return;
-    const macRegex = /([0-9A-Fa-f]{1,2}[:-]){5}([0-9A-Fa-f]{1,2})/;
-    const ipRegex = /\\b\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\b/;
-    stdout.split('\\n').forEach(line => {
-      const macM = line.match(macRegex);
-      const ipM = line.match(ipRegex);
-      if (macM && ipM) {
-        const mac = macM[0].replace(/-/g, ':').toLowerCase();
-        if (mac !== 'ff:ff:ff:ff:ff:ff') deviceDb.upsertDevice({ mac_address: mac, ip_address: ipM[0] });
+    stdout.split('\n').forEach(line => {
+      const fields = line.trim().split(/\s+/);
+      const lladdr = fields.indexOf('lladdr');
+      if (fields[0] && lladdr >= 0 && fields[lladdr + 1]) {
+        const mac = fields[lladdr + 1].toLowerCase();
+        if (mac !== 'ff:ff:ff:ff:ff:ff') {
+          deviceDb.upsertDevice({ mac_address: mac, ip_address: fields[0] });
+        }
       }
     });
     deviceDb.markOffline();
